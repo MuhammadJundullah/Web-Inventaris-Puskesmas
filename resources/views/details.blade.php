cf1d85008d05409cce40b41f8842d0bd10a03970
Muhammad Jundullah <sayidmuhammad15@gmail.com>
2024-10-18 11:33:19 +0700

    update

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -34,11 +34,11 @@
 {{-- modal --}}
 
 {{-- isi --}}
-    <div class="m-10 mx-5 ">
+    <div class="m-10 sm:mx-20 sm:px-5">
 
     {{-- modal berhasil --}}
         @if (session("success"))
-        <div role="alert" class="rounded border-s-4 border-green-500 bg-green-50 p-4">
+        <div role="alert" class="rounded border-s-4 border-green-500 bg-green-50 p-4 mb-5">
                 <span class="text-green-600 flex">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
