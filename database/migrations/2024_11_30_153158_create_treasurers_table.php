f552249ef878963070c2b7888dc50ee03657a8c5
radeniss <mohammad.220170208@mhs.unimal.ac.id>
2024-12-05 16:56:31 +0700

    add bendahara 2

--- database/migrations/2024_11_30_153158_create_treasurers_table.php
+++ database/migrations/2024_11_30_153158_create_treasurers_table.php
@@ -17,8 +17,8 @@ return new class extends Migration
             $table->string('id_pegawai')->nullable();
             $table->string('tanggal')->nullable();
             $table->string('kegiatan')->nullable();
-            $table->string('dana_yang_digunakan')->nullable();
-            $table->string('jumlah')->nullable();
+            $table->integer('dana_yang_digunakan')->nullable();
+            $table->integer('jumlah')->nullable();
             $table->timestamps();
         });
     }
