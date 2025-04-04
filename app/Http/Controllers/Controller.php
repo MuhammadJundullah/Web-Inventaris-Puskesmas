d1f72b85b255ead54bad8b58a14c2ae308c1e561
Muhammad Jundullah <sayidmuhammad15@gmail.com>
2025-04-05 06:39:06 +0700

    update

--- app/Http/Controllers/Controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Http\Controllers;
-use App\Models\inventory;
-
-abstract class Controller
-{
-
-}
--- resources/views/components/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer class="bg-gray-800 text-white py-4">
-        <div class="container mx-auto text-center sm:text-sm text-xs">
-            <p>&copy; 2024 Mahasiswa Kerja Praktek UNIMAL. Semua Hak Dilindungi.</p>
-        </div>
-</footer>
